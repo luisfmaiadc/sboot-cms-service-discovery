34ebb47d476fe8d62d98abc9e8ec929fdbf1bab9
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-18 23:09:26 -0300

    Inicialização do versionamento do Service Discovery.

---
+++ src/main/java/com/portfolio/luisfmdc/sboot_cms_service_discovery/SbootCmsServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_cms_service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootCmsServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootCmsServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/sboot_cms_service_discovery/SbootCmsServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_cms_service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootCmsServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
