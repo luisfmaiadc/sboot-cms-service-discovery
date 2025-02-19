9a179b729b8fe667590fc152ec0c2e5c8e14a6bb
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-18 23:20:58 -0300

    Modificações no pom.xml e application.properties e habilitação do service discovery em Application.

--- src/main/java/com/portfolio/luisfmdc/sboot_cms_service_discovery/SbootServiceDiscoveryApplication.java
+++ src/main/java/com/portfolio/luisfmdc/sboot_cms_service_discovery/SbootServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.portfolio.luisfmdc.sboot_cms_service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class SbootCmsServiceDiscoveryApplication {
+@EnableEurekaServer
+public class SbootServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbootCmsServiceDiscoveryApplication.class, args);
+		SpringApplication.run(SbootServiceDiscoveryApplication.class, args);
 	}
 
 }
